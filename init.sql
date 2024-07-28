fe2429fabecd1b401bd63720141e9e6aee07ddbc
Favower <Amir.umakhanov@yandex.ru>
2024-07-29 02:04:24 +0300

    Up

--- init.sql
+++ init.sql
@@ -1,9 +1,25 @@
-CREATE USER myuser WITH PASSWORD 'mypassword';
-CREATE DATABASE messages_db;
-GRANT ALL PRIVILEGES ON DATABASE messages_db TO myuser;
+-- Create the user if it does not exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'myuser') THEN
+        CREATE ROLE myuser WITH LOGIN PASSWORD 'mypassword';
+    END IF;
+END
+$$;
 
-\connect messages_db
-CREATE TABLE messages (
+-- Create the database if it does not exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'messages_db') THEN
+        CREATE DATABASE messages_db WITH OWNER = myuser;
+    END IF;
+END
+$$;
+
+-- Connect to the database and create the table
+\c messages_db;
+
+CREATE TABLE IF NOT EXISTS messages (
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
     processed BOOLEAN NOT NULL DEFAULT FALSE,
